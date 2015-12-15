1b579aa93452a72dae945cb503e794b04f885f9d
clement <clement.vion49@gmail.com>
2015-12-15 13:46:02 +0100

    record

--- js/script.js
+++ js/script.js
@@ -1,36 +1,55 @@
-console.log("yo");
-
 window.URL = window.URL || window.webkitURL;
-   
-   navigator.getUserMedia = (navigator.getUserMedia || 
-                             navigator.webkitGetUserMedia || 
-                             navigator.mozGetUserMedia || 
-                             navigator.msGetUserMedia);
-   var v = document.getElementById('v');
-   var cameraStream = '';
-   if (navigator.getUserMedia) {
-      // Request access to video only
-      navigator.getUserMedia(
-         {
-            video:true,
-            audio:true
-         },        
-         function(stream) {
-            cameraStream = stream;
-            v.src = window.URL.createObjectURL(stream);
-            
-         },
-         function() {
-            document.writeln("problem with accessing the hardware")
-         }
-      );
-   }
-   else {
-      document.writeln("Video capture is not supported");
-   }
-   document.querySelector('#stopbt').addEventListener('click', 
-    function(e){
-      v.src='';
-      cameraStream.stop();
+
+navigator.getUserMedia = (navigator.getUserMedia || 
+                         navigator.webkitGetUserMedia || 
+                         navigator.mozGetUserMedia || 
+                         navigator.msGetUserMedia);
+
+var video = document.querySelector('#video'),
+    cameraStream,
+    audio = document.querySelector('#audio');
+
+if (navigator.getUserMedia) {
+    // Request access to video only
+    navigator.getUserMedia(
+    {
+        video:true,
+        audio:false
+    },        
+    function(stream) {
+        cameraStream = stream;
+        video.src = window.URL.createObjectURL(stream); 
+        
+        document.querySelector('#startButton').addEventListener('click', function(){
+
+            var mediaRecorder = new MediaStreamRecorder(stream);
+            mediaRecorder.mimeType = 'video/webm';
+
+            mediaRecorder.width = 320;
+            mediaRecorder.height = 240;
+
+            mediaRecorder.ondataavailable = function (blob) {
+                // POST/PUT "Blob" using FormData/XHR2
+                var blobURL = URL.createObjectURL(blob);
+                document.write('<a href="' + blobURL + '" target="_blank" >url</a>');
+            };
+
+            var duration = audio.duration*1000;
+
+            mediaRecorder.start(duration);
+            setTimeout(function(){mediaRecorder.stop();}, duration);
+
+            audio.play();
+        }); 
+        },
+    function() {
+        document.writeln("problem with accessing the hardware")
     });
-    
+}
+else {
+    document.writeln("Video capture is not supported");
+}
+
+
+
+
